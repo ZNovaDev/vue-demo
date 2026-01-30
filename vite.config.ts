332faf1d01ec078f916c88981117097fa2789e61
nova <nova_xyz@163.com>
2026-01-30 11:49:34 +0800

    fix: 调整打包策略

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { createHtmlPlugin } from 'vite-plugin-html'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 
@@ -21,11 +20,11 @@ export default defineConfig(({ command }) => {
         resolvers: [
           // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
           ElementPlusResolver(),
-        // 自动导入图标组件
-        IconsResolver({
-          prefix: 'Icon',
-        }),
-      ],
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
         imports: ['vue', 'vue-router', 'pinia'],
         dts: true,
       }),
@@ -64,19 +63,48 @@ export default defineConfig(({ command }) => {
     },
     base: './',
     build: {
-      reportCompressedSize: false,
       // 消除打包大小超过500kb警告
-      chunkSizeWarningLimit: 2000000,
+      chunkSizeWarningLimit: 2000,
       minify: 'esbuild',
-      assetsDir: 'static/assets',
-      // 静态资源打包到dist下的不同目录
       rollupOptions: {
         output: {
-          chunkFileNames: 'static/js/[name]-[hash].js',
-          entryFileNames: 'static/js/[name]-[hash].js',
-          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
-        },
-      },
+          // 核心分包逻辑：只判断路径，不计算大小
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // 将node_modules中的模块按"类目"分到不同桶里
+              // 返回相同的字符串=装进同一个chunk文件
+              if (id.includes('element-plus')) {
+                return 'ui'         // UI库装一起
+              }
+              if (id.includes('echarts') || id.includes('chart')) {
+                return 'charts'     // 图表库装一起
+              }
+              if (id.includes('vue') || id.includes('pinia') || id.includes('vue-router')) {
+                return 'vue'        // Vue生态装一起
+              }
+
+              return 'vendor'       // 其他的全装vendor里
+            }
+          },
+          chunkFileNames: 'static/js/[name]-[hash:8].js',
+          entryFileNames: 'static/js/[name]-[hash:8].js',
+          // assetFileNames: 'static/[ext]/[name]-[hash:8].[ext]',
+          assetFileNames: (chunkInfo) => {
+            const name = chunkInfo.names[0]
+            if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(name)) {
+              return 'static/img/[name]-[hash:8][extname]'
+            }
+            if (/\.(woff2?|eot|ttf|otf)$/i.test(name)) {
+              return 'static/font/[name]-[hash:8][extname]'
+            }
+            if (name?.endsWith('.css')) {
+              return 'static/css/[name]-[hash:8][extname]'
+            }
+            return 'static/assets/[name]-[hash:8][extname]'
+          }
+        }
+      }
+
     },
   }
 })
